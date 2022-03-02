3f98f538f9324ebe4ecbececf75667e6bb7cc1b1
hasantezcan <hasantezcan77@gmail.com>
2022-03-03 01:35:53 +0300

    Add google form

--- pages/Main/index.tsx
+++ pages/Main/index.tsx
@@ -9,6 +9,17 @@ const Main = () => {
       <Header />
       <NextEventCard />
       <PreviousEventList />
+
+      <div className="form">
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLScCjzsICzKWqa16i4urzSGUheRq-Cx2BJTiLDd7fSSNrVnRyw/viewform?embedded=true"
+          frameBorder="0"
+          className="speaker-form-iframe"
+        >
+          Yükleniyor…
+        </iframe>
+      </div>
+
       <Footer />
     </main>
   );
